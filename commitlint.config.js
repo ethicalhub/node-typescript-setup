704f89677f1fff0205e8b189b27e0c557dfb59fe
ethicalhub <yasiransari.elith@gmail.com>
2025-06-16 05:12:31 +0530

    feat: commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,32 @@
+// commitlint.config.js
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'build',
+        'revert',
+        'wip',
+        'release',
+        'config',
+        'types',
+      ],
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case']],
+    'subject-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 72],
+  },
+};
