3eac93486865c1bf74720ba92cd33b96a76023a3
ethicalhub <yasiransari.elith@gmail.com>
2025-06-16 15:07:49 +0530

    feat: setup prettier with eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,32 +1,16 @@
 // commitlint.config.js
 export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'chore',
-        'perf',
-        'ci',
-        'build',
-        'revert',
-        'wip',
-        'release',
-        'config',
-        'types',
-      ],
-    ],
-    'subject-case': [2, 'never', ['start-case', 'pascal-case']],
-    'subject-empty': [2, 'never'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'header-max-length': [2, 'always', 72],
-  },
-};
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf', 'ci', 'build', 'revert', 'wip', 'release', 'config', 'types']
+        ],
+        'subject-case': [2, 'never', ['start-case', 'pascal-case']],
+        'subject-empty': [2, 'never'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'header-max-length': [2, 'always', 72]
+    }
+}
--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,22 @@
 // eslint.config.js
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-   {
-    rules: {
-      'no-console': 'error',
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+        rules: {
+            'no-console': 'error',
+            'no-unused-vars': 'warn',
+            'no-undef': 'error',
+            quotes: ['error', 'single', { allowTemplateLiterals: true }]
+        }
     },
-  },
 
-  {
-     ignores: ['dist/**', 'node_modules/**'],
-  }
-);
+    {
+        ignores: ['dist/**', 'node_modules/**']
+    }
+)
